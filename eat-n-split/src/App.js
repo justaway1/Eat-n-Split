790ff8c3961a1dd7dfeebe1410a482aabc8a7233
Stanislav Angelov <stannyweb@gmail.com>
2023-12-19 08:50:22 +0200

    Added UI for Friends

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -5,7 +5,12 @@ const randomFriends = [
     img: 'https://i.pravatar.cc/48?u=118836',
     balance: 10
   },
-  { id: 2, name: 'Kate', img: 'https://i.pravatar.cc/48?u=118834', balance: 0 },
+  {
+    id: 2,
+    name: 'Kate',
+    img: 'https://i.pravatar.cc/48?u=118834',
+    balance: 0
+  },
   {
     id: 3,
     name: 'Pablo',
@@ -24,4 +29,34 @@ export default function App () {
   )
 }
 
-function FriendList () {}
+function FriendList () {
+  const friendList = randomFriends
+  return (
+    <ul>
+      {friendList.map(friend => {
+        return <Friend friend={friend} key={friend.id} />
+      })}
+    </ul>
+  )
+}
+
+function Friend ({ friend }) {
+  return (
+    <li>
+      <img src={friend.img} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p
+        className={
+          friend.balance < 0 ? 'red' : friend.balance > 0 ? 'green' : ''
+        }
+      >
+        {friend.balance < 0
+          ? `You owe ${friend.name} ${Math.abs(friend.balance)}$`
+          : friend.balance === 0
+          ? `You and ${friend.name} are even`
+          : `${friend.name} owes you ${friend.balance}$`}
+      </p>
+      <button className='button'>Select</button>
+    </li>
+  )
+}
