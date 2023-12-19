b571ce4eae5793084cffbca3ab8c2aa845ec0872
Stanislav Angelov <stannyweb@gmail.com>
2023-12-19 08:36:08 +0200

    Added Initial Friends

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -1,3 +1,27 @@
+const randomFriends = [
+  {
+    id: 1,
+    name: 'Stan',
+    img: 'https://i.pravatar.cc/48?u=118836',
+    balance: 10
+  },
+  { id: 2, name: 'Kate', img: 'https://i.pravatar.cc/48?u=118834', balance: 0 },
+  {
+    id: 3,
+    name: 'Pablo',
+    img: 'https://i.pravatar.cc/48?u=118832',
+    balance: -1
+  }
+]
+
 export default function App () {
-  return <h1>Hello</h1>
+  return (
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendList />
+      </div>
+    </div>
+  )
 }
+
+function FriendList () {}
